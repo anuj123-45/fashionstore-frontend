f40b9605a6878f8be21679d383469ed93f2717f9
anuj123-45 <anuj1358.be20@chikara.edu.in>
2023-10-30 14:52:52 +0530

    changes done

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -47,7 +47,6 @@ import {
       const config = { headers:{ "Content-Type":"application/json" } };
   
       const { data } = await axios.post(`/api/v1/login`,{email,password },config);
-  
       dispatch({ type: LOGIN_SUCCESS,payload:data.user});
     } catch (error) {
       dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
